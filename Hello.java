94eb24a4c3e4bc7bbf7d2025896822917c03697d
sachinsah09 <ssa.axelor@gmail.com>
2019-06-06 18:39:47 +0530

    first change in master

--- Hello.java
+++ Hello.java
@@ -5,19 +5,5 @@ public static void main()
 {
 
 
-
-
-<<<<<<< HEAD
-=======
-=======
-hello commit 1
-Styetm.out.trintln("line added in master branch");
->>>>>>> parent of b679863... check 1
->>>>>>> parent of d650ae8... all 2
-
-
-
-update 1 in master
-
-
-Update 2 in master
+changes in master
+first
