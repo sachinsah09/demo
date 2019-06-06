737cb0d3abaca1da4a54d3d79765bb2951cc3f4b
sachinsah09 <ssa.axelor@gmail.com>
2019-06-06 13:28:34 +0530

    all

--- Hello.java
+++ Hello.java
@@ -7,4 +7,11 @@ public static void main()
 
 
 
+<<<<<<< HEAD
+=======
+=======
+hello commit 1
+Styetm.out.trintln("line added in master branch");
+>>>>>>> parent of b679863... check 1
+>>>>>>> parent of d650ae8... all 2
 
